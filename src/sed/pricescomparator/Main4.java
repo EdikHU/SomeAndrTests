2b643c8f724a95079b299aad43a11086289b7aeb
Deck Y <work@win.ds>
2014-03-07 15:08:34 +0400

    two activity (Main4 delay bettwin start activity 2)

--- src/sed/pricescomparator/Main4.java
+++ src/sed/pricescomparator/Main4.java
@@ -1,10 +1,13 @@
 package sed.pricescomparator;
 
-import android.app.Activity;
-import android.os.Bundle;
+import java.util.Date;
 
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
 import android.view.View;
 import android.view.View.OnClickListener;
+import android.widget.Button;
 import android.widget.LinearLayout;
 import android.widget.TextView;
 
@@ -12,13 +15,18 @@ public class Main4 extends Activity{
 
 	private LinearLayout layout;
 	private TextView tv;
+	private Main4 instance;
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
+		System.out.println("*** onCreate");
+		
+		instance = this;
 		
 		layout = new LinearLayout(this);
 		setContentView(layout );
+		layout.setOrientation(LinearLayout.VERTICAL);
 
 		layout.setOnClickListener(new OnClickListener() {
 			
@@ -26,14 +34,77 @@ public class Main4 extends Activity{
 			public void onClick(View v) {
 				int x = layout.getWidth();
 				int y = layout.getHeight();
-				tv.setText("["+x+"]["+y+"]");
+				tv.setText(""+new Date()+"["+x+"]["+y+"]");
 			}
 		});
 
 		tv =new TextView(this);
 		layout.addView(tv);
+		
+		Button buttExplictCall = new Button(this);
+		buttExplictCall.setText("explict call");
+		layout.addView(buttExplictCall);
+		buttExplictCall.setOnClickListener(new OnClickListener() {
+			
+			@Override
+			public void onClick(View v) {
+				
+				startActivity( new Intent( instance, Main4Activity2.class));
+				
+			}
+		});
+		
+		Button buttImplictCall = new Button(this);
+		buttImplictCall.setText("HZ Call");
+		layout.addView(buttImplictCall);
+		buttImplictCall.setOnClickListener(new OnClickListener() {
+			
+			@Override
+			public void onClick(View v) {
+				
+				startActivity( new Intent("sed.pricescomparator.M4ACT2"));
+				
+			}
+		});
+
+		
 	}
+
 	
-	
+//	@Override
+//	protected void onStart() {
+//		super.onStart();
+//		System.out.println("*** onStart");
+//	}
+//
+//	@Override
+//	protected void onResume() {
+//		super.onResume();
+//		System.out.println("*** onResume");
+//	}
+//
+//	
+//	// inited and showed
+//
+//	
+//	@Override
+//	protected void onPause() {
+//		super.onPause();
+//		System.out.println("*** onPause");
+//	}
+//	
+//
+//	@Override
+//	protected void onStop() {
+//		super.onStop();
+//		System.out.println("*** onStop");
+//	}
+//	
+//	
+//	@Override
+//	protected void onDestroy() {
+//		super.onDestroy();
+//		System.out.println("*** onDestroy");
+//	}
 
 }
---
+++ src/sed/pricescomparator/Main4Activity2.java
@@ -0,0 +1,112 @@
+package sed.pricescomparator;
+
+import java.util.Date;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.Button;
+import android.widget.LinearLayout;
+import android.widget.TextView;
+
+public class Main4Activity2 extends Activity{
+
+	private Main4Activity2 instance;
+	private boolean needClose = false;
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		System.out.println("*   A2  onCreate");
+
+		instance = this;
+		
+		LinearLayout layout = new LinearLayout(this);
+		layout.setOrientation(LinearLayout.VERTICAL);
+		setContentView(layout);
+		
+		TextView tv2 = new TextView(this);
+		tv2.setText("ch M4A2 "+new Date());
+		
+		layout.addView(tv2);
+		
+		Button butt = new Button(this);
+		butt.setText("back");
+		layout.addView(butt);
+		butt.setOnClickListener(new OnClickListener() {
+			
+			@Override
+			public void onClick(View v) {
+				
+				startActivity( new Intent( instance, Main4.class));
+				
+			}
+		});
+		
+		new Thread(){
+			@Override
+			public void run() {
+				super.run();
+				System.out.println("here");
+				try {
+					Thread.sleep(4321);
+					startActivity( new Intent( instance, Main4.class));
+				} catch (InterruptedException e) {
+					e.printStackTrace();
+				}
+				System.out.println("here out");
+			}
+		}.start();
+	}
+
+
+
+	@Override
+	protected void onStart() {
+		super.onStart();
+		System.out.println("**  A2  onStart");
+	}
+	
+	@Override
+	protected void onResume() {
+		super.onResume();
+		System.out.println("*** A2  onResume");
+		if (needClose){
+			System.out.println("NEED CLOSE HERE");
+			instance.finish();
+		}
+	}
+
+	
+
+	@Override
+	protected void onPause() {
+		super.onPause();
+		System.out.println("*** A2  onPause");
+	}
+
+	@Override
+	protected void onStop() {
+		super.onStop();
+		System.out.println("**  A2  onStop");
+		needClose = true;
+	}
+
+	@Override
+	protected void onDestroy() {
+		super.onDestroy();
+		System.out.println("*   A2  onDestroy");
+	}
+
+
+
+
+
+
+
+
+
+}
