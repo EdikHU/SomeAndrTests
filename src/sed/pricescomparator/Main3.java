e1475f61eeb6c9609022d2a0da8e9c439ea2183a
Deck Y <work@win.ds>
2014-03-11 12:46:49 +0400

    not changed view... we can use Thread().start().

--- src/sed/pricescomparator/Main3.java
+++ src/sed/pricescomparator/Main3.java
@@ -1,6 +1,7 @@
 package sed.pricescomparator;
 
 import java.util.ArrayList;
+import java.util.Iterator;
 
 import android.app.Activity;
 import android.os.Bundle;
@@ -25,11 +26,15 @@ public class Main3 extends Activity{
 		
 		Button btnPrn = new Button(this);
 		layout.addView(btnPrn);
+		btnPrn.setText("long process");
 		btnPrn.setOnClickListener(new OnClickListener() {
 			
 			@Override
 			public void onClick(View v) {
 				System.out.println(al);
+				System.out.println("long process");
+				longProcessing(14000);
+				System.out.println("waiter finish");
 			}
 		});
 		
@@ -68,6 +73,28 @@ public class Main3 extends Activity{
 		listView.setAdapter(adapter);
 		
 		
+	}
+
+	protected void longProcessing(final int i) {
+		new Thread(){
+			@Override
+			public void run() {
+				try {
+					System.out.println("longProcessing start ["+i+"]");
+					Thread.sleep(1234);
+					Iterator<Main3Item> iter = al.iterator();
+					while (iter.hasNext()){
+						Main3Item elm = iter.next();
+						elm.checked = (elm.checked ? false:true);
+					}
+					//
+					System.out.println("longProcessing finish ["+i+"]");
+				} catch (InterruptedException e) {
+					e.printStackTrace();
+				}
+			}
+		}.start();
+		adapter.notifyDataSetChanged();
 	}
 
 }
