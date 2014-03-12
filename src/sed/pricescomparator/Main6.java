f751f43e6a2753f06d580a54d1bbf65ba4fc9a4e
Deck Y <work@win.ds>
2014-03-12 13:07:01 +0400

    orientation / disabling onCreate (in manifest(activity) need add a:configChanges, or a:screenOrientation; in activity _can_ be present onConfigurationChanged and onSaveInstanceState)

--- src/sed/pricescomparator/Main6.java
+++ src/sed/pricescomparator/Main6.java
@@ -3,6 +3,7 @@ package sed.pricescomparator;
 import java.io.Serializable;
 import android.app.Activity;
 import android.app.Fragment;
+import android.content.res.Configuration;
 import android.os.Bundle;
 import android.view.View;
 import android.view.View.OnClickListener;
@@ -34,12 +35,9 @@ public class Main6 extends Activity {
 		});
 		la.addView(btn);
 
-//		 Object[] obj = (Object[]) getLastNonConfigurationInstance();
-//		 if (obj != null && obj.length == 2){
-//		 s1 = (SomeS1) obj[0];
-//		 s2 = (SomeS2) obj[1]; 
-//		 }
+		System.out.println("HERE LOAD check for restoring");
 		if (state != null) {
+			System.out.println("HERE LOAD restoring ["+state+"]");
 			s1 = (SomeS1) state.getSerializable("s1");
 			s2 = (SomeS2) state.getSerializable("s2");
 		}
@@ -67,50 +65,25 @@ public class Main6 extends Activity {
 		la.addView(tvs2);
 	}
 
-//	 @Override
-//	 @Deprecated
-//	 public Object onRetainNonConfigurationInstance() {
-//	 Object[] objs = new Object[2];
-//	 objs[0] = s1;
-//	 objs[1] = s2;
-//	 return objs;
-//	 }
 
-	
-
-	
-	
 	@Override
-	protected void onSaveInstanceState(Bundle ost) {
+	protected void onSaveInstanceState(Bundle ost) { 
 		super.onSaveInstanceState(ost);
-		System.out.println("HERE1");
+		System.out.println("HERE SAVE onSaveInstanceState");
 		ost.putSerializable("s1", s1);
 		ost.putSerializable("s2", s2);
-		System.out.println("HERE2");
 	}
-
-	
-	
-//	@Override
-//	protected void onDestroy() {
-//		super.onDestroy();
-//		System.out.println("HERE DESTROY");
-//	}
-
-
 
 	@Override
-	protected void onRestoreInstanceState(Bundle sis) {
-		super.onRestoreInstanceState(sis);
-		if (sis != null){
-			s1 = (SomeS1) sis.getSerializable("s1");
-			s2 = (SomeS2) sis.getSerializable("s2");
-		}
+	public void onConfigurationChanged(Configuration newConfig) {
+		System.out.println("HERE !!! CALL onConfigurationChanged");
+		super.onConfigurationChanged(newConfig);
 	}
+	
 
 
 
-	class SomeS1 implements Serializable{
+	private static class SomeS1 implements Serializable {
 		private static final long serialVersionUID = 7624549534285865793L;
 		String id = "some1 ";
 		int cnt = 0;
@@ -126,7 +99,7 @@ public class Main6 extends Activity {
 		}
 	}
 
-	class SomeS2  implements Serializable{
+	private static class SomeS2 implements Serializable {
 		private static final long serialVersionUID = -5462642597414324743L;
 		String id = "some2 ";
 		int cnt = 0;
@@ -141,4 +114,5 @@ public class Main6 extends Activity {
 			return "[" + id + " " + cnt + "]";
 		}
 	}
+
 }
