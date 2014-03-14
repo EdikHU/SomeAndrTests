1c4335c8c93f72df1a4ec66ef4a0bbc6060baaa6
Ed H <hm@win.asb>
2014-03-14 08:06:31 +0400

    initial service 3

--- src/sed/pricescomparator/Main9.java
+++ src/sed/pricescomparator/Main9.java
@@ -1,7 +1,33 @@
 package sed.pricescomparator;
 
 import android.app.Activity;
+import android.os.Bundle;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.Button;
+import android.widget.LinearLayout;
 
 public class Main9 extends Activity{
 
+	private Button btn;
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		LinearLayout la = new LinearLayout(this);
+		la.setOrientation(LinearLayout.VERTICAL);
+		
+		btn = new Button(this);
+		la.addView(btn);
+		btn.setOnClickListener(new OnClickListener() {
+			@Override
+			public void onClick(View v) {
+				System.out.println("here");
+			}
+		});
+		
+	}
+
+	
+	
 }
